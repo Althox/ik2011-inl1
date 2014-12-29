113f8fb09cdcc226f2e86f2fa733beef79d36bc3
Toppe <Toppe@192.168.1.7>
2014-12-29 21:15:39 +0100

    adding classes

---
+++ ik2011-inl1/src/java/controller/AdminBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Toppe
+ */
+@ManagedBean(name = "adminBean")
+@SessionScoped
+public class AdminBean implements Serializable{
+    
+}
