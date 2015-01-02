c04ae9184457557be7a8dd9b29d0c2e800e8552d
Martin <Martin@192.168.1.13>
2015-01-02 21:06:10 +0100

    almost done with loggin

--- ik2011-inl1/src/java/DAL/UserDAO.java
+++ ik2011-inl1/src/java/DAL/UserDAO.java
@@ -13,7 +13,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import model.Team;
 import model.User;
+import model.UserRole;
 
 /**
  *
@@ -25,9 +28,65 @@ public class UserDAO extends DAO implements Serializable {
     }
     
     public User login(String username, String password) throws SQLException {
-        PreparedStatement stmnt = con.prepareCall("SELECT * FROM user WHERE username = ?");
+        CallableStatement stmt = con.prepareCall("{ call p_login('"+username+"', '"+password+"') }");
+        ResultSet rs = stmt.executeQuery();
         
-        return null;
+        User user = new User();    
+        int team_id = 0;
+        int role_id = 0;
+        
+        while(rs.next()){
+            user.setId(rs.getInt("user_id"));
+            user.setUsername(rs.getString("username"));
+            role_id = rs.getInt("role_id");
+            team_id = rs.getInt("team_id");
+        }
+        
+        UserRole role = new UserRole();
+        Team team = new Team();
+        role = this.getRole();
+        team = this.getTeam();
+        
+        if(role_id == role.getId()){
+            user.setRole(role);
+        }
+        if(team_id == team.getId()){
+            user.setAssociatedTeam(team);
+        }
+        
+        return user;
+    }
+    
+    private Team getTeam(){
+        Team team = new Team();
+        try{
+            PreparedStatement stmt = con.prepareCall(" SELECT * FROM team ");
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                team.setId(rs.getInt("team_id"));
+                team.setName(rs.getString("name"));
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return team;
+    }
+    
+    private UserRole getRole(){
+        UserRole role = new UserRole();
+        
+        try{
+            PreparedStatement stmt = con.prepareCall(" SELECT * FROM user_role ");
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                role.setId(rs.getInt("role_id"));
+                role.setName(rs.getString("name"));
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        return role;
     }
     
     public void createUser(String username, String password) throws SQLException {
@@ -38,4 +97,7 @@ public class UserDAO extends DAO implements Serializable {
         stmt.executeQuery();
         ResultSet rs = stmt.getResultSet();
     }
+    
+
+    
 }
